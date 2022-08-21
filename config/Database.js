9e239883d4f9e0519e7190241cc0a40125b584cf
inayah99 <kuni.inayah44@gmail.com>
2022-08-21 19:21:59 +0700

    deploy backend

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize('resepsharing_db', 'root', 'R4has1a99!',{
-    host: "127.0.0.1",
+    host: "localhost",
     dialect: "mysql"
 });
 
